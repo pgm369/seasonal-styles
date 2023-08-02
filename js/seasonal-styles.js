f849bc31949611af5b1e10217d248369952d30f4
Priscila Garcia-Mendoza <noreply>
2023-08-02 10:38:56 -0700

    updates index

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -1,37 +1,46 @@
-$(document).ready(function () {
-  $('.seasons a').click(function (e) {
-    e.preventDefault();
-    let season = $(this).attr("href");
-    season = season.toLowerCase();
+$("document").ready(function(){
+    $('.seasons a').click(function(e){//find all a tags inside class of seasons
+        e.preventDefault();//stop default submission
+        let season = $(this).attr("href");//contents of href attribute of this element
+        //alert(season);
 
-    switch (season) {
-      case "spring":
-        $("html").css("background-color", "#2B7129");
-        $("#slogan").text("Spring into our Spring wear!");
-        $("#wear").attr("src", "images/spring-wear.jpg");
-        $("#logo").attr("src", "images/spring.gif");
+        season = season.toLowerCase();
+
+        switch(season){
+            case "spring":
+                $("html").css("background-color","#2B7129");
+                $("#slogan").text("Spring into our spring wear!")
+                $("#wear").attr("src","images/spring-wear.jpg");
+                $("#logo").attr("src","images/spring.gif");
         break;
 
-      case "summer":
-        $("html").css("background-color", "#EBA52B");
-        $("#slogan").text("Summer fits!");
-        $("#wear").attr("src", "images/summer-wear.jpg");
-        $("#logo").attr("src", "images/summer.gif");
+        case "summer":
+            $("html").css("background-color", "#EBA52B");
+            $("#slogan").text("It's getting hot in here!");
+            $("#wear").attr("src", "images/summer-wear.jpg");
+            $("#logo").attr("src", "images/summer.gif");
         break;
 
-      case "winter":
-        $("html").css("background-color", "#005393");
-        $("#slogan").text("Walk into Winter wear!");
-        $("#wear").attr("src", "images/winter-wear.jpg");
-        $("#logo").attr("src", "images/winter.gif");
+        case "winter":
+            $("html").css("background-color", "#005393");
+            $("#slogan").text("BRRRR It's so cold!!");
+            $("#wear").attr("src", "images/winter-wear.jpg");
+            $("#logo").attr("src", "images/winter.gif");
+        break;
+    
+        case "fall":
+            $("html").css("background-color", "#A81124");
+            $("#slogan").text("The leaves are falling!!!!");
+            $("#wear").attr("src", "images/fall-wear.jpg");
+            $("#logo").attr("src", "images/fall.gif");
         break;
 
-      case "fall":
-        $("html").css("background-color", "#A81124");
-        $("#slogan").text("Fall into the best fit!");
-        $("#wear").attr("src", "images/fall-wear.jpg");
-        $("#logo").attr("src", "images/fall.gif");
+        case "default":
+            $("html").css("background-color", ""); 
+            $("#slogan").text("Outfitter for All Seasons!");
+            $("#wear").attr("src", "images/300x400.png");
+            $("#logo").attr("src", "images/four-seasons.gif");
         break;
-    }
-  });
+        }
+    });
 });
