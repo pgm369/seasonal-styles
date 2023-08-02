1d13694d7336493d54d2132ffdc3fd39ed16c9f4
Priscila Garcia-Mendoza <noreply>
2023-08-02 10:52:45 -0700

    updates index

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -9,35 +9,35 @@ $("document").ready(function(){
         switch(season){
             case "spring":
                 $("html").css("background-color","#2B7129");
-                $("#slogan").text("Spring into our spring wear!")
+                $("#slogan").text("Spring into our variety of clothing for cold and warm weather!")
                 $("#wear").attr("src","images/spring-wear.jpg");
                 $("#logo").attr("src","images/spring.gif");
         break;
 
         case "summer":
             $("html").css("background-color", "#EBA52B");
-            $("#slogan").text("It's getting hot in here!");
+            $("#slogan").text("Summer is here! Shopping begins!");
             $("#wear").attr("src", "images/summer-wear.jpg");
             $("#logo").attr("src", "images/summer.gif");
         break;
 
         case "winter":
             $("html").css("background-color", "#005393");
-            $("#slogan").text("BRRRR It's so cold!!");
+            $("#slogan").text("Happy Holidays! Something for everyone in this cold breeze!");
             $("#wear").attr("src", "images/winter-wear.jpg");
             $("#logo").attr("src", "images/winter.gif");
         break;
     
         case "fall":
             $("html").css("background-color", "#A81124");
-            $("#slogan").text("The leaves are falling!!!!");
+            $("#slogan").text("Pumpkin Spice and everything nice!");
             $("#wear").attr("src", "images/fall-wear.jpg");
             $("#logo").attr("src", "images/fall.gif");
         break;
 
         case "default":
             $("html").css("background-color", ""); 
-            $("#slogan").text("Outfitter for All Seasons!");
+            $("#slogan").text("Outfitter for Every Season!");
             $("#wear").attr("src", "images/300x400.png");
             $("#logo").attr("src", "images/four-seasons.gif");
         break;
